8db1ba0a00ff021d282f852ba289d6ab8b59e07c
MariaFernandaMancilla <barrientosmancillam@gmail.com>
2026-01-08 21:33:46 -0600

    entrega fcfs

--- src/schdl/fcfs.c
+++ src/schdl/fcfs.c
@@ -10,8 +10,24 @@ int main() {
     read_processes(p, n);
     init_processes(p, n);
 
-    // TODO: Aquí va la lógica del scheduler
-    
+    int time = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (time < p[i].arrival_time) {
+            time = p[i].arrival_time;
+        }
+
+        printf("Tiempo %d: Ejecutando P%d (BT=%d)\n",
+               time, p[i].id, p[i].burst_time);
+
+        p[i].waiting_time = time - p[i].arrival_time;
+        time += p[i].burst_time;
+        p[i].turnaround_time = p[i].waiting_time + p[i].burst_time;
+        p[i].completed = 1;
+
+        printf("   -> P%d terminó en tiempo %d\n", p[i].id, time);
+    }
+
     print_results(p, n, "FCFS Scheduling");
     return 0;
 }
